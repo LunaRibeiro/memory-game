4a84e279e2475c3d38c82ca6443a308c2dd4da78
luna.as <luna.as@itaipuparquetec.org.br>
2026-05-21 14:38:51 -0300

    chore: Initialize Memory Game project with React and Vite setup.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+      <main className="app">
+        <h1>Memory Game</h1>
+        <p>Projeto iniciado com React + Vite.</p>
+      </main>
+  )
+}
+
+export default App
